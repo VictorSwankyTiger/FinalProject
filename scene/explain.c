838b10f6c1e41a74688cdbbf403ea5339ae8b822
vito <mchung110\7@gmail.com>
2024-06-10 21:22:57 +0800

    version6.1.3

--- scene/explain.c
+++ scene/explain.c
@@ -10,13 +10,13 @@ Scene *New_Explain(int label)
     Scene *pObj = New_Scene(label);
     // setting derived object member
     pDerivedObj->background = al_load_bitmap("assets/image/control.png");
-    pDerivedObj->font = al_load_ttf_font("assets/font/pirulen.ttf", 40, 0);
+    pDerivedObj->font = al_load_ttf_font("assets/font/pirulen.ttf", 20, 0);
     // Load sound
-    pDerivedObj->song = al_load_sample("assets/sound/menu.mp3");
+    pDerivedObj->song = al_load_sample("assets/sound/bgm.mp3");
     al_reserve_samples(20);
     pDerivedObj->sample_instance = al_create_sample_instance(pDerivedObj->song);
     pDerivedObj->title_x = WIDTH / 2;
-    pDerivedObj->title_y = HEIGHT / 2;
+    pDerivedObj->title_y = HEIGHT / 6 * 5;
     // Loop the song until the display closes
     al_set_sample_instance_playmode(pDerivedObj->sample_instance, ALLEGRO_PLAYMODE_LOOP);
     al_restore_default_mixer();
@@ -46,11 +46,11 @@ void Explain_draw(Scene *self)
     al_clear_to_color(al_map_rgb(0, 0, 0));
     Explain *gs = ((Explain *)(self->pDerivedObj));
     al_draw_bitmap(gs->background, 0, 0, 0);
-    al_draw_text(gs->font, al_map_rgb(0, 0, 0), 800, 785, ALLEGRO_ALIGN_CENTRE, "Press Esc to leave");
-    ALLEGRO_BITMAP *logout = al_load_bitmap("assets/image/logout.png");
-    al_draw_bitmap(logout, 5, 780, 0);
-    //al_draw_text(Obj->font, al_map_rgb(255, 255, 255), Obj->title_x, Obj->title_y, ALLEGRO_ALIGN_CENTRE, "Press \"Esc\" to escape");
-    //al_draw_rectangle(Obj->title_x - 150, Obj->title_y - 30, Obj->title_x + 150, Obj->title_y + 30, al_map_rgb(255, 255, 255), 0);
+    al_draw_text(gs->font, al_map_rgb(0, 0, 0),  Obj->title_x,  Obj->title_y, ALLEGRO_ALIGN_CENTRE, "Press Esc to leave");
+    // ALLEGRO_BITMAP *logout = al_load_bitmap("assets/image/logout.png");
+    // al_draw_bitmap(logout, 5, 780, 0);
+    // al_draw_text(Obj->font, al_map_rgb(255, 255, 255), Obj->title_x, Obj->title_y, ALLEGRO_ALIGN_CENTRE, "Press \"Esc\" to escape");
+    // al_draw_rectangle(Obj->title_x - 150, Obj->title_y - 30, Obj->title_x + 150, Obj->title_y + 30, al_map_rgb(255, 255, 255), 0);
     al_play_sample_instance(Obj->sample_instance);
 }
 void Explain_destroy(Scene *self)
